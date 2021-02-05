d6b638bf0710e15fe7e134dbad091f623e481516
Mirfozil <72867640+Mirfozil2000@users.noreply.github.com>
2021-02-05 15:20:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// var num = +prompt('Введите число:');
+// var number = +prompt('Введите степень:');
+// var answer = num;
+// var count = num;
+
+
+// while (count <= numb){
+//   answer = answer * number;
+//   console.log(answer);
+//   alert(answer);
+//   count++;
+// } первый скрипт
+
+// var number = +prompt('Введите число');
+// var object = prompt('Введите второе');
+// var subject = prompt('Введите третье');
+// if (steps = isNaN){
+//     steps = 12
+// }
+// for (let i = 1; i < steps; i++) {
+//   var row = object.repeat(i) + subject;
+//   console.log(row);
+//   alert(row)    
+// } третий скрипт
+
